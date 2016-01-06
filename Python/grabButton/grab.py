6337547a054327b3bd02aceb194f736ffe112f33
radames <radamajna@gmail.com>
2016-01-06 17:06:28 +0000

    fixed string filename error

--- Python/grabButton/grab.py
+++ Python/grabButton/grab.py
@@ -19,30 +19,33 @@ if __name__ == "__main__":
     print "Setting up button"
     time.sleep(1)
     screen = pygame.display.set_mode(list(camera.resolution))
+    
+    bytesStream = io.BytesIO()
 	
-	bytesStream = io.BytesIO()
-	try:
-		print "Press the button to grab a picture:"
-		button.wait_for_press()
-		camera.capture(bytesStream, format='jpeg')
-		data = np.fromstring(bytesStream.getvalue(), dtype=np.uint8)
-		image = cv2.imdecode(data,1)
+    try:
+        print "Press the button to grab a picture:"
+        button.wait_for_press()
 
-        print "Image File saved as ", fname
+        camera.capture(bytesStream, format='jpeg')
+        data = np.fromstring(bytesStream.getvalue(), dtype=np.uint8)
+        image = cv2.imdecode(data,1)
+
+        fname = 'PIC-' + str(int(time.time())) + '.jpg'
+        print "Image File saved as: ", fname
         cv2.imwrite(fname,image)
 
         camera.stop_preview()
-		
-		pygame.init()
-		screen.fill([0,0,0])
-		frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-		frame = np.rot90(frame)
-		frame = pygame.surfarray.make_surface(frame)
-		screen.blit(frame, (0,0))
-		pygame.display.update()
 
-		time.sleep(5)
-		sys.exit(0)
+        pygame.init()
+        screen.fill([0,0,0])
+        frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        frame = np.rot90(frame)
+        frame = pygame.surfarray.make_surface(frame)
+        screen.blit(frame, (0,0))
+        pygame.display.update()
+
+        time.sleep(5)
+        sys.exit(0)
     except KeyboardInterrupt, SystemExit:
         camera.stop_preview()
             
