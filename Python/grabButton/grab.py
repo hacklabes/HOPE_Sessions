95b753a809b6f92f64ce6e82d434ad27e2dac450
Radamés Ajna <radamajna@gmail.com>
2016-01-06 16:57:06 +0000

    changed to preview with pygame

--- Python/grabButton/grab.py
+++ Python/grabButton/grab.py
@@ -2,6 +2,11 @@ import time
 import picamera
 from gpiozero import Button
 import sys
+import numpy as np
+import cv2
+import pygame
+import io
+
 
 if __name__ == "__main__":
 
@@ -13,15 +18,31 @@ if __name__ == "__main__":
     button = Button(pin=6, pull_up=False)
     print "Setting up button"
     time.sleep(1)
-    try:
-        while True:
-            if button.is_pressed:
-                fname = 'PIC-'+str(int(time.time()))+'.jpg' 
-                camera.capture(fname)
-                camera.stop_preview()
-                print "Image File saved as ", fname
-                time.sleep(1)
-                sys.exit(0)
+    screen = pygame.display.set_mode(list(camera.resolution))
+	
+	bytesStream = io.BytesIO()
+	try:
+		print "Press the button to grab a picture:"
+		button.wait_for_press()
+		camera.capture(bytesStream, format='jpeg')
+		data = np.fromstring(bytesStream.getvalue(), dtype=np.uint8)
+		image = cv2.imdecode(data,1)
+
+        print "Image File saved as ", fname
+        cv2.imwrite(fname,image)
+
+        camera.stop_preview()
+		
+		pygame.init()
+		screen.fill([0,0,0])
+		frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		frame = np.rot90(frame)
+		frame = pygame.surfarray.make_surface(frame)
+		screen.blit(frame, (0,0))
+		pygame.display.update()
+
+		time.sleep(5)
+		sys.exit(0)
     except KeyboardInterrupt, SystemExit:
         camera.stop_preview()
             
