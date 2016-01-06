197b168c96c715dde744be99c1b47dccefa30eac
Thiago Hersan <thiago.hersan@gmail.com>
2016-01-06 16:57:05 +0000

    removes pygame and fixes exit for videoButton script

--- Python/videoButton/videoButton.py
+++ Python/videoButton/videoButton.py
@@ -3,20 +3,17 @@
 
 from pyomxplayer import OMXPlayer
 from gpiozero import LED, Button
-import pygame
 import time
 import sys
 
 omx = OMXPlayer('../../Scratch/movs/MechanicalPrinciples.mov')
 
-pygame.init()
-screen = pygame.display.set_mode((640,480))
-screen.fill((0,0,0))
-
 yButton = Button(6,  pull_up=False, bounce_time=0.05)
 yLed = LED(20)
 yButton.when_pressed = yLed.on
 yButton.when_released = yLed.off
+
+rButton = Button(19,  pull_up=False, bounce_time=0.05)
                                  
 try:
     while True:
@@ -24,5 +21,8 @@ try:
             omx.toggle_pause()
         if not (yButton.is_pressed or omx.paused):
             omx.toggle_pause()
+	if rButton.is_pressed:
+	    omx.stop()
+	    sys.exit()
 except KeyboardInterrupt:
         sys.exit()
