3a9acdac5dc5e3bb632d321a303d204ce9b10ba4
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-01-07 00:16:34 +0000

    makes camera code more consistent with other code (removes name==main, etc)

--- 02_Clairvoyance/grabButton/grab.py
+++ 02_Clairvoyance/grabButton/grab.py
@@ -7,45 +7,44 @@ import cv2
 import pygame
 import io
 
+print "Setting up Camera"
+time.sleep(1)
+camera = picamera.PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview(alpha=200)
 
-if __name__ == "__main__":
+print "Setting up button"
+button = Button(pin=6, pull_up=False, bounce_time=0.01)
+time.sleep(1)
 
-    print "Setting up Camera"
-    time.sleep(1)     
-    camera = picamera.PiCamera()
-    camera.resolution = (1024, 768)
-    camera.start_preview(alpha=200) 
-    button = Button(pin=6, pull_up=False)
-    print "Setting up button"
-    time.sleep(1)
-    screen = pygame.display.set_mode(list(camera.resolution))
-    
-    bytesStream = io.BytesIO()
-	
-    try:
-        print "Press the button to grab a picture:"
-        button.wait_for_press()
+screen = pygame.display.set_mode(list(camera.resolution))
 
-        camera.capture(bytesStream, format='jpeg')
-        data = np.fromstring(bytesStream.getvalue(), dtype=np.uint8)
-        image = cv2.imdecode(data,1)
+bytesStream = io.BytesIO()
 
-        fname = 'PIC-' + str(int(time.time())) + '.jpg'
-        print "Image File saved as: ", fname
-        cv2.imwrite(fname,image)
+try:
+    print "Press the button to grab a picture:"
+    button.wait_for_press()
 
-        camera.stop_preview()
+    camera.capture(bytesStream, format='jpeg')
+    data = np.fromstring(bytesStream.getvalue(), dtype=np.uint8)
+    image = cv2.imdecode(data,1)
 
-        pygame.init()
-        screen.fill([0,0,0])
-        frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        frame = np.rot90(frame)
-        frame = pygame.surfarray.make_surface(frame)
-        screen.blit(frame, (0,0))
-        pygame.display.update()
+    fname = 'PIC-' + str(int(time.time())) + '.jpg'
+    print "Image File saved as: ", fname
+    cv2.imwrite(fname,image)
 
-        time.sleep(5)
-        sys.exit(0)
-    except KeyboardInterrupt, SystemExit:
-        camera.stop_preview()
-            
+    camera.stop_preview()
+
+    pygame.init()
+    screen.fill([0,0,0])
+    frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    frame = np.rot90(frame)
+    frame = pygame.surfarray.make_surface(frame)
+    screen.blit(frame, (0,0))
+    pygame.display.update()
+
+    time.sleep(5)
+    sys.exit(0)
+except KeyboardInterrupt, SystemExit:
+    camera.stop_preview()
+    sys.exit(0)
--- 02_Clairvoyance/grabTwoButton/grab2blend.py
+++ 02_Clairvoyance/grabTwoButton/grab2blend.py
@@ -2,59 +2,60 @@ import time
 import picamera
 from gpiozero import Button
 import sys
-import io
 import numpy as np
 import cv2
 import pygame
+import io
 
-if __name__ == "__main__":
+print "Setting up Camera"
+time.sleep(1)
+camera = picamera.PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview(alpha=200)
 
-    print "Setting up Camera"
-    time.sleep(1)     
-    camera = picamera.PiCamera()
-    camera.resolution = (1024, 768)
-    camera.start_preview(alpha=200) 
-    button = Button(pin=6, pull_up=False)
-    print "Setting up button"
-    time.sleep(1)
-    bytesStream = io.BytesIO()
+print "Setting up button"
+button = Button(pin=6, pull_up=False, bounce_time=0.01)
+time.sleep(1)
 
-    screen = pygame.display.set_mode(list(camera.resolution))
-    try:
-        
-        print "Press the button for the first shot:"
-        button.wait_for_press()
-        bytesStream.seek(0)
-        camera.capture(bytesStream, format='jpeg')
-        data = np.fromstring(bytesStream.getvalue(), dtype=np.uint8)
-        image1 = cv2.imdecode(data,1)
-                
-        print "Press the button for the second shot:"
-        button.wait_for_press()
-        bytesStream.seek(0)
-        camera.capture(bytesStream, format='jpeg')
-        data = np.fromstring(bytesStream.getvalue(), dtype=np.uint8)
-        image2 = cv2.imdecode(data,1)
-        
-        imageBlend = cv2.addWeighted(image1, 0.8, image2, 0.5, 0) 
-        
+screen = pygame.display.set_mode(list(camera.resolution))
 
-        fname = 'PIC-'+str(int(time.time()))+'.jpg'
-        print "Image File saved as ", fname
-        cv2.imwrite(fname,imageBlend)
+bytesStream = io.BytesIO()
 
-        camera.stop_preview()
+try:
+    print "Press the button for the first shot:"
+    button.wait_for_press()
+    bytesStream.seek(0)
 
-        pygame.init()
-        screen.fill([0,0,0])
-        frame = cv2.cvtColor(imageBlend, cv2.COLOR_BGR2RGB)
-        frame = np.rot90(frame)
-        frame = pygame.surfarray.make_surface(frame)
-        screen.blit(frame, (0,0))
-        pygame.display.update()
+    camera.capture(bytesStream, format='jpeg')
+    data = np.fromstring(bytesStream.getvalue(), dtype=np.uint8)
+    image1 = cv2.imdecode(data,1)
 
-        time.sleep(10)
-        sys.exit(0)
-    except KeyboardInterrupt, SystemExit:
-        camera.stop_preview()
-            
+    print "Press the button for the second shot:"
+    button.wait_for_press()
+    bytesStream.seek(0)
+
+    camera.capture(bytesStream, format='jpeg')
+    data = np.fromstring(bytesStream.getvalue(), dtype=np.uint8)
+    image2 = cv2.imdecode(data,1)
+
+    imageBlend = cv2.addWeighted(image1, 0.8, image2, 0.5, 0) 
+
+    fname = 'PIC-'+str(int(time.time()))+'.jpg'
+    print "Image File saved as ", fname
+    cv2.imwrite(fname,imageBlend)
+
+    camera.stop_preview()
+
+    pygame.init()
+    screen.fill([0,0,0])
+    frame = cv2.cvtColor(imageBlend, cv2.COLOR_BGR2RGB)
+    frame = np.rot90(frame)
+    frame = pygame.surfarray.make_surface(frame)
+    screen.blit(frame, (0,0))
+    pygame.display.update()
+
+    time.sleep(10)
+    sys.exit(0)
+except KeyboardInterrupt, SystemExit:
+    camera.stop_preview()
+    sys.exit(0)
