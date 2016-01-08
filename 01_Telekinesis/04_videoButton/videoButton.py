1dfd65f186957fa7fd9950d4f597f840c1265fb7
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-01-08 16:02:13 +0000

    adds exceptions to videoButton script

--- 01_Telekinesis/04_videoButton/videoButton.py
+++ 01_Telekinesis/04_videoButton/videoButton.py
@@ -19,16 +19,21 @@ yButton.when_released = yLed.off
 # we'll use another button for quitting the program (since it runs fullscreen)
 rButton = Button(19,  pull_up=False, bounce_time=0.05)
 
-# run this next section forever, until user stops the programme by pressing button
-while True:
-    # play video if button is pressed and video player is currently paused
-    if yButton.is_pressed and omx.paused:
-        omx.toggle_pause()
-    # pause video if button is not pressed and player is not paused
-    if (not yButton.is_pressed) and (not omx.paused):
-        omx.toggle_pause()
+try:
+    # run this next section forever, until user stops the programme by pressing button
+    while True:
+        # play video if button is pressed and video player is currently paused
+        if yButton.is_pressed and omx.paused:
+            omx.toggle_pause()
+        # pause video if button is not pressed and player is not paused
+        if (not yButton.is_pressed) and (not omx.paused):
+            omx.toggle_pause()
 
-    # when quit button is pressed, stop player and quit programme
-    if rButton.is_pressed:
-        omx.stop()
+        # when quit button is pressed, stop player and quit programme
+        if rButton.is_pressed:
+            omx.stop()
+            sys.exit(0)
+
+# this is some magic code that detects when user hits ctrl-c (and stops the programme)
+except KeyboardInterrupt, SystemExit, RuntimeError:
         sys.exit(0)
