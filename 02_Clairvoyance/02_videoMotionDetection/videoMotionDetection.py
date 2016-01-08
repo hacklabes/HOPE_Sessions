06035203d6d1ebd9e39047536d3a649423ca4180
Radamés Ajna <radamajna@gmail.com>
2016-01-08 13:53:43 +0000

    motion detection with simple graph of diff average between frames

--- 02_Clairvoyance/02_videoMotionDetection/videoMotionDetection.py
+++ 02_Clairvoyance/02_videoMotionDetection/videoMotionDetection.py
@@ -15,16 +15,22 @@ def diffImg(t0, t1, t2):
 camera = cv2.VideoCapture(0)
 pygame.init()
 pygame.display.set_caption("OpenCV camera stream on Pygame")
-screen = pygame.display.set_mode([1280,720])
+
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode([screen_width, screen_height])
+
+threshold = 500
 
 try:
     t1 = cv2.cvtColor(camera.read()[1], cv2.COLOR_RGB2GRAY)
     t2 = cv2.cvtColor(camera.read()[1], cv2.COLOR_RGB2GRAY)
     t3 = cv2.cvtColor(camera.read()[1], cv2.COLOR_RGB2GRAY)
 
-    runMean = []
+    runMean = np.random.uniform(9000.0, 50935.0,50).tolist()
     ind = 0
     lastMean = 0
+    points = runMean
     while True:
         frame = camera.read()[1]
 
@@ -34,21 +40,26 @@ try:
         t3 = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
         frame = diffImg(t1,t2,t3)
-        if len(runMean) < 100:
-            runMean.append(cv2.countNonZero(frame))
-        else:
-           runMean[ind] = cv2.countNonZero(frame)
-           ind = (ind+1)%len(runMean)
+        runMean[ind] = cv2.countNonZero(frame)
         mean = np.mean(runMean)
-        if abs(lastMean - mean) > 200:
-            print "MOTION", abs(lastMean-mean)
+        points[ind]=abs(mean-lastMean)
+        xypoints = points[:ind] + len(points[ind:])*[0]
+        ind = (ind+1)%len(runMean)
         lastMean = mean
 
+        if points[ind] > threshold:
+            print "MOTION", points[ind]
         frame = diffImg(t1,t2,t3)
         frame = np.rot90(frame)
         frame = pygame.surfarray.make_surface(frame)
         screen.fill([0,0,0])
         screen.blit(frame, (0,0))
+
+        xStep = float(screen_width) / len(xypoints)
+        xypoints = [[i*xStep, 400 - v/3] for i, v in enumerate(xypoints)]
+
+        pygame.draw.line(screen, [255,0,0], [0, 400 - threshold/3], [screen_width,400 - threshold/3], 5)
+        pygame.draw.lines(screen, [0,255,0], False, xypoints, 5)
 
         pygame.display.update()
         for event in pygame.event.get():
