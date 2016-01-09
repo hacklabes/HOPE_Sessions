753af1a0c0147f52e610c18f53ea8d20ad4be061
Radamés Ajna <radamajna@gmail.com>
2016-01-09 13:09:38 +0000

    broadcast function on the server

--- 02_Clairvoyance/04_voyeurChat/aClient.py
+++ 02_Clairvoyance/04_voyeurChat/aClient.py
@@ -7,56 +7,63 @@ import numpy as np
 import pygame
 
 
-if __name__ == "__main__":
-	# Connect a client socket to my_server:8000 (change my_server to the
-	# hostname of your server)
-	client_socket = socket.socket()
-	client_socket.connect(('localhost', 8000))
-	screen = pygame.display.set_mode([1920,720])
-	pygame.init()  # Initialize pygame
-	
-	try:
-		while True:
-			userInput = raw_input(" > ")
-				
-			if userInput != '':
-				if userInput.split()[0] == "C" and len(userInput.split()) == 1:
-					print "CAPTURE"
-					client_socket.sendall("C")
-					img_len = struct.unpack('<L', client_socket.recv(struct.calcsize('<L')))[0]
-					if img_len > 0:
-						data = ''
-						while len(data) < img_len:
-							packet = client_socket.recv(img_len-len(data))
-							if not packet:
-								break
-							data += packet
+# Connect a client socket to my_server:8000 (change my_server to the
+# hostname of your server)
 
-					img_stream = io.BytesIO()
-					img_stream.write(data)
-					
-					#img_stream.write(client_socket.recv(img_len))
-					# Rewind the stream, open it as an image with PIL and do some
-					# processing on it
-					img_stream.seek(0)
-					img_array = np.fromstring(img_stream.getvalue(), dtype=np.uint8)
-					imgCV = cv2.imdecode(img_array,1)
-#					cv2.imshow("SERVER",imgCV)
-					screen.fill([0, 0, 0])  # Blank fill the screen
-					frame=cv2.cvtColor(imgCV,cv2.COLOR_BGR2RGB)
-					frame=np.rot90(frame)
-					frame=pygame.surfarray.make_surface(frame) #I think the color error lies in this line
+if len(sys.argv) == 1:
+    print "Please add you nickname as argument"
+    sys.exit(0)
+nickname = str(sys.argv[1])
+print "Welcome ", nickname
 
-					screen.blit(frame, (0, 0))  # Load new image on screen
-					pygame.display.update()  # Update pygame display
+client_socket = socket.socket()
+client_socket.connect(('localhost', 8000))
+screen = pygame.display.set_mode([1920,720])
 
-				elif userInput.split()[0] == "E" and len(userInput.split()) == 1:
-					client_socket.sendall("E");
-					sys.exit(0)
-				else:
-					client_socket.sendall(userInput);
-			else:
-				print "TYPE SOMETHING or C for capture or E for exit"
-				
-	except KeyboardInterrupt, SystemExit:
-		client_socket.close()
+pygame.init()  # Initialize pygame
+
+client_socket.sendall(nickname.strip())
+try:
+    while True:
+        userInput = raw_input(" > ")
+        if userInput != '':
+            if userInput.split()[0] == "C" and len(userInput.split()) == 1:
+                print "CAPTURE"
+                client_socket.sendall("C")
+                img_len = struct.unpack('<L', client_socket.recv(struct.calcsize('<L')))[0]
+                if img_len > 0:
+                    data = ''
+                    while len(data) < img_len:
+                        packet = client_socket.recv(img_len-len(data))
+                        if not packet:
+                            break
+                        data += packet
+
+                img_stream = io.BytesIO()
+                img_stream.write(data)
+
+                #img_stream.write(client_socket.recv(img_len))
+                # Rewind the stream, open it as an image with PIL and do some
+                # processing on it
+                img_stream.seek(0)
+                img_array = np.fromstring(img_stream.getvalue(), dtype=np.uint8)
+                imgCV = cv2.imdecode(img_array,1)
+#                   cv2.imshow("SERVER",imgCV)
+                screen.fill([0, 0, 0])  # Blank fill the screen
+                frame=cv2.cvtColor(imgCV,cv2.COLOR_BGR2RGB)
+                frame=np.rot90(frame)
+                frame=pygame.surfarray.make_surface(frame) #I think the color error lies in this line
+
+                screen.blit(frame, (0, 0))  # Load new image on screen
+                pygame.display.update()  # Update pygame display
+
+            elif userInput.split()[0] == "E" and len(userInput.split()) == 1:
+                client_socket.sendall("E");
+                sys.exit(0)
+            else:
+                client_socket.sendall(userInput);
+        else:
+            print "TYPE SOMETHING or C for capture or E for exit"
+
+except KeyboardInterrupt, SystemExit:
+    client_socket.close()
--- 02_Clairvoyance/04_voyeurChat/aServer.py
+++ 02_Clairvoyance/04_voyeurChat/aServer.py
@@ -1,62 +1,94 @@
+import threading
 import SocketServer
 import io
-import socket
 import struct
 import cv2
-import threading
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+    def updateInfor(self):
+        self.server.clients_info = []
+        for client in self.server.clients:
+            self.server.clients_info.append(client.info)
 
-	def handle(self):
-		cur_thread = threading.current_thread()
-		response = "{}".format(cur_thread.name)
-		print "{} wrote:".format(self.client_address[0])
+    def addClient(self,client):
+        print "Add client to the list", client
+        self.server.clients.append(client)
 
-		while True:
-			self.data = self.request.recv(1024)
-			if self.data:
-				self.dt = self.data.strip()
-				if self.dt[0]=="E" and len(self.dt)== 1:
-					print "CLOSING CLIENT"
-					break
-				elif self.dt[0]=="C" and len(self.dt)== 1:
-					print "SENDING IMAGE"
-					self.ret, self.frame = capture.read()
-					self.encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
-#					self.frame =  cv2.resize(self.frame, (0,0), fx=0.3, fy=0.3) 
-					self.result, self.imgencoded = cv2.imencode('.jpg', self.frame, self.encode_param)
-					
-					self.stream = io.BytesIO()
-					self.stream.write(self.imgencoded)
-					self.request.sendall(struct.pack('<L', len(self.stream.getvalue())))
-					self.stream.seek(0)
-					self.request.sendall(self.stream.read())
-				#	self.request.sendall(struct.pack('<L', 0))
-				else:
-					print self.data
+    def handle(self):
+        print "Clients> ", self.client_address
+        while True:
+            data = self.request.recv(1024)
+            if data:
+                dt = data.strip()
+                if dt[0]=="E" and len(dt)== 1:
+                    print "CLOSING CLIENT"
+                    break
+                elif dt[0]=="C" and len(dt)== 1:
+                    print "SENDING IMAGE"
+                    self.server.list_clients()
+
+                    ret, frame = capture.read()
+                    encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
+    #                   self.frame =  cv2.resize(self.frame, (0,0), fx=0.3, fy=0.3)
+                    result, imgencoded = cv2.imencode('.jpg', frame, encode_param)
+                    stream = io.BytesIO()
+                    stream.write(imgencoded)
+                    self.request.sendall(struct.pack('<L', len(stream.getvalue())))
+                    stream.seek(0)
+                    self.request.sendall(stream.read())
+                    #   self.request.sendall(struct.pack('<L', 0))
+                else:
+                    print data
+    def removeClient(self,client):
+        print "Remove client", client
+        try:
+            self.server.clients.remove(client)
+            self.updateInfo()
+        except Exception:
+            pass
 
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-	pass
+    daemon_threads = True
+    allow_reuse_address = True
 
-if __name__ == "__main__":
-	try:
-		# Port 0 means to select an arbitrary unused port
-		HOST, PORT = "localhost", 8000
+    def __init__(self, address, handler):
+        SocketServer.TCPServer.__init__(self, address, handler)
+        self.clients = []
+        self.clients_info =[]
+    def broadcast(self, data):
+        for c in self.clients:
+            try:
+                c.send(data)
+            except:
+                self.clients.remove(c)
 
-		server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-		ip, port = server.server_address
+    def list_clients(self):
+        return self.clients_info
 
-		# Start a thread with the server -- that thread will then start one
-		# more thread for each request
-		server_thread = threading.Thread(target=server.serve_forever)
-		# Exit the server thread when the main thread terminates
-		server_thread.daemon = True
-		server_thread.start()
-		print "Server loop running in thread:", server_thread.name
-		capture = cv2.VideoCapture(0)
-		server.serve_forever()
-	except KeyboardInterrupt:
-		capture.release()
-		server.shutdown()
-		server.server_close()
+    def shutdown(self):
+        for c in self.clients:
+            c.exit_event.set()
+        SocketServer.TCPServer.shutdown(self)
+
+
+try:
+    # Port 0 means to select an arbitrary unused port
+    HOST, PORT = "localhost", 8000
+
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    ip, port = server.server_address
+    # Start a thread with the server -- that thread will then start one
+    # more thread for each request
+    server_thread = threading.Thread(target=server.serve_forever)
+    # Exit the server thread when the main thread terminates
+    server_thread.daemon = True
+    server_thread.start()
+    print "Server loop running in thread:", server_thread.name
+    capture = cv2.VideoCapture(0)
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    capture.release()
+    server.shutdown()
+    server.server_close()
