795abcffff3ebf76c117072897845fbb1c74dab2
Radamés Ajna <radamajna@gmail.com>
2016-01-10 04:11:17 +0000

    chat with messages working

--- 02_Clairvoyance/04_voyeurChat/nClient.py
+++ 02_Clairvoyance/04_voyeurChat/nClient.py
@@ -5,70 +5,89 @@ import sys
 import cv2
 import numpy as np
 import pygame
-import thread
-
+from threading import Thread
+import readline
 
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 
-if len(sys.argv) == 1:
-    print "Please add you nickname as argument"
+try:
+    client_socket = socket.socket()
+    client_socket.connect(('localhost', 8000))
+except Exception as e:
+    print e.errno, e.strerror
     sys.exit(0)
 
-nickname = str(sys.argv[1])
+print "Welcome to the voyeurChat"
+print "Start sending your nickname"
+#screen = pygame.display.set_mode([1920,720])
+#pygame.init()  # Initialize pygame
 
-client_socket = socket.socket()
-client_socket.connect(('localhost', 8000))
-client_socket.sendall(nickname.strip())
-data = client_socket.recv(1024)
-
-if data.split()[0] == "\N":
-    print "The Nickname already exist, try again"
-    client_socket.close()
-    sys.exit(0)
-
-print "Welcome ", nickname
-screen = pygame.display.set_mode([1920,720])
-pygame.init()  # Initialize pygame
+while True:
+    try:
+        nickname = raw_input("Insert Nickname > ")
+        if len(nickname.strip()) > 3:
+            client_socket.sendall("\N")
+            client_socket.sendall(nickname.strip())
+            data = client_socket.recv(1024)
+            if data.split()[0] == "\N":
+                print "The Nickname already exist or it's to short, try again"
+            elif data.split()[0] == "OK":
+                break
+        else:
+            print "Nickname must be longer"
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 def waitPackages():
     while True:
         try:
             data = client_socket.recv(1024)
-            if data !='':
+            if len(data) > 0:
                 dt = data.split()
                 if dt[0] == "\C":
                     #capture
                     print "CAPTURING"
                 else:
+                    sys.stdout.write('\r'+' '*(len(readline.get_line_buffer())+2)+'\r')
                     print data
-        except Exception, SystemExit:
-            print "STOP client thread"
+                    sys.stdout.write('> ' + readline.get_line_buffer())
+                    sys.stdout.flush()
 
-thread.start_new_thread(waitPackages,())
-try:
-    while True:
+        except Exception:
+            print "STOP client thread"
+            break
+
+t = Thread(target = waitPackages)
+t.daemon = True
+t.start()
+
+while True:
+    try:
         userInput = raw_input("> ")
-        if userInput != '':
+        userInput = userInput.strip()
+        if len(userInput) > 0:
             dt = userInput.split()
             if dt[0] == "\C":
                 if len(dt) >= 2:
-                    msgTo = dt[1]
-                    print "CAPTURE ", msgTo
-                    client_socket.sendall("\C "+ msgTo)
+                    nickname = dt[1]
+                    print "CAPTURE ", nickname
+                    client_socket.sendall("\C")
+                    client_socket.sendall(nickname)
                     #----now wait for the IMAGE
                 else:
                     print "TYPE \C YourFriendNickname"
             elif dt[0]  == "\E":
+                #send disconnection
                 client_socket.sendall("\E")
                 raise SystemExit
             else:
                 client_socket.sendall(userInput);
         else:
+            print "\n"
             print "Type your message or:"
             print "\C nickname for capture"
             print "\L list all connected"
             print "\E for exit"
-
-except KeyboardInterrupt, SystemExit:
-    client_socket.close()
+    except KeyboardInterrupt, SystemExit:
+        client_socket.close()
--- 02_Clairvoyance/04_voyeurChat/nServer.py
+++ 02_Clairvoyance/04_voyeurChat/nServer.py
@@ -12,37 +12,57 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
         self.connection = self.request
         self.ip = self.client_address[0]
         self.port = self.client_address[1]
-        self.nickname = self.request.recv(1024).strip() #first data is the nickname
-        error = self.server.addClient(self)
-        if error:
-            log.info("Nickname error")
+        self.nickname = self.ip
+        if self.server.nicknameExist(self.nickname):
+            log.info("Client Exist error")
             self.connection.sendall("\N") #nickname errro
-            return
+            return False
+        self.server.addClient(self)
         log.info("Client connect - %s", self.client_address)
 
-        msg = self.nickname + "<- Connected\n"
+        while True:
+            try:
+                data = self.request.recv(1024).strip()
+                if data.split()[0] == "\N":
+                    nn = self.request.recv(1024).strip()
+                    if not self.server.nicknameExist(nn) and len(nn) > 3:
+                        log.info("Updated nickname - %s", nn)
+                        self.server.updateNickname(self, nn)
+                        self.connection.sendall("OK") #nickname errr
+                        break
+                    else:
+                        self.connection.sendall("\N")
+                        log.info("Erro updating nickname")
+            except:
+                log.info("Error gettin nickname")
+                break
+
+        msg = self.nickname + "> Connected"
         self.server.sendBroadcast(self, msg)
+
         while True:
             try:
                 data = self.request.recv(1024)
                 if data:
                     dt = data.strip().split()
-                    if len(dt) >= 1:
+                    if len(dt) > 1:
                         if dt[0] == "\C":
-                            msgTo = dt[1]
-                            if not msgTo:
-                                self.server.list_clients()
+                            nickname = self.request.recv(1024)
+                            if self.server.nicknameExist(nickname):
                                 log.info("Capture - %s", msgTo)
                         elif dt[0] == "\L":
                             #send list of users for the client as request
-                            list = reduce(lambda x,y: x + "\n" + y, self.server.clients.keys())
+                            list = "==== Users ====\n"
+                            list += reduce(lambda x,y: x + "\n" + y, self.server.clients.keys())
+                            list += "\n==============="
                             self.connection.sendall(list)
                         elif dt[0] == "\E":
-                            log.info("Closing Client - %s", self)
+                            self.server.sendBroadcast(self, self.nickname + "> Disconnected")
+                            log.info("Closing Client - %s", self.client_address)
                             self.server.removeClient(self)
                             break
                         else:
-                            msg = self.nickname + "> " + data + "\n"
+                            msg = self.nickname + "> " + data
                             self.server.sendBroadcast(self, msg)
             except Exception:
                 log.info("Client connection error")
@@ -56,23 +76,27 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
         self.clients = {}
         SocketServer.TCPServer.__init__(self, address, handler)
 
-    def addClient(self, client):
-        if self.clients.has_key(client.nickname):
-            return True #send error
-        self.clients[client.nickname] = client
-        return False #no error
+    def updateNickname(self, client, nickname):
+        self.clients[nickname] = self.clients.pop(client.nickname)
+        client.nickname = nickname
 
     def removeClient(self,client):
         del self.clients[client.nickname]
 
-    def nicknameExist(self,client):
-        return self.clients.has_key(client.nickname)
+    def nicknameExist(self, nickname):
+        return self.clients.has_key(nickname)
 
     def sendBroadcast(self,client, data):
         for c in self.clients.itervalues():
             if c.nickname != client.nickname:
-                print c.nickname
                 c.request.sendall(data)
+
+    def addClient(self, client):
+        if self.nicknameExist(client.nickname):
+            return True #send error
+        self.clients[client.nickname] = client
+        return False #no error
+
 
 
 try:
